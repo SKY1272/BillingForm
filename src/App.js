0a8595011cd3ebb6491c3412735d1f40c22f6fcd
SKY <88556477+SKY1272@users.noreply.github.com>
2023-04-14 10:04:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import BillingFrom from "./components/BillingForm";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <BillingFrom />
+    </div>
+  );
+}
